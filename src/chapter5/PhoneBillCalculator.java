0651f3cfce39a1ed409eeac197f8a6578194cc3b
stanim01 <stanim01@stepstone.com>
2019-09-26 11:20:29 +0200

    first commit

---
+++ src/chapter5/PhoneBillCalculator.java
@@ -0,0 +1,23 @@
+package chapter5;
+
+/*
+Calculate the final total of someone's cell phone bill.
+Allow the operator to input the plan fee and the number of overage minutes.
+Charge the user 0.25 for every minute they were over their plan, and 15% tax on the subtotal.
+Create separate methods to calculate the tax, overage fees, and final total.
+Print the itemized bill.
+ */
+
+
+public class PhoneBillCalculator {
+    public static void main(String[] arg) {
+        //what we know for sure
+
+        //what we don't know and need to get
+
+        //what we need to count
+
+        //what we need to inform
+
+    }
+}
